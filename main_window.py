67ff02afbc1726e12daf0b8b4ae7219120a07039
m1ntMoonspell <zy1752706779@gmail.com>
2025-12-30 11:42:34 +0800

    added a new Slice Window

--- main_window.py
+++ main_window.py
@@ -1,5 +1,45 @@
-from PySide6.QtWidgets import QMainWindow
+from PySide6.QtWidgets import (QPushButton,QFileDialog,
+                               QMessageBox,QApplication,QVBoxLayout,
+                               QLabel,QDialog)
+import csv,os
+from pathlib import Path
 
-class MyWindow(QMainWindow):
+
+class MyWindow(QDialog):
     def __init__(self):
         super().__init__()
+        self.creat_subwidgets()
+        self.setWindowTitle("Trace Helper")
+
+    def creat_subwidgets(self):
+        self.file_button = QPushButton("Select...",self)
+        self.file_button.clicked.connect(self.open_files)
+        self.slice_button = QPushButton("Slice...",self)
+        self.slice_button.clicked.connect(self.slice_phrase)
+        self.label = QLabel("All Rights Reserved" \
+        "<a href='https://github.com/m1ntMoonspell?tab=repositories'>@m1nt</a>",
+        self)
+        self.label.setOpenExternalLinks(True)
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.file_button)
+        layout.addWidget(self.label)
+        self.setLayout(layout)
+
+    def slice_phrase(self):
+        
+
+    def open_files(self):
+        fileName,_ = QFileDialog.getOpenFileName(self)
+        suffix = os.path.splitext(".")[1]
+        if fileName and suffix == ".csv":
+            pass
+        elif fileName and suffix != ".csv":
+            QMessageBox.critical(self,"Error","please choose trace sheet")
+        else:
+            pass
+
+if __name__ == "__main__":
+    app = QApplication()
+    window = MyWindow()
+    window.show()
+    app.exec()
---
+++ slice_dialog.py
@@ -0,0 +1,44 @@
+from PySide6.QtWidgets import (QLineEdit,QDialog,QPushButton,
+                               QApplication,QVBoxLayout,QMessageBox,
+                               QHBoxLayout,QLabel)
+from PySide6.QtGui import QGuiApplication 
+
+class SliceDia(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.edit = QLineEdit(placeholderText="Enter text here")
+        self.button = QPushButton("Slice!")
+        self.button.clicked.connect(self.get_text)
+        self.clear_button = QPushButton("Clear")
+        self.clear_button.clicked.connect(self.edit.clear)
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.edit)
+        layout.addWidget(self.button)
+        layout.addWidget(self.clear_button)
+        self.setWindowTitle("Link Slicer")
+        hl = QHBoxLayout()
+        self.status_label = QLabel("Status:")
+        self.status = QLabel("Null")
+        hl.addWidget(self.status_label)
+        hl.addWidget(self.status)
+        hl.addStretch()
+        layout.addLayout(hl)
+
+    def get_text(self):
+        text = self.edit.text()
+        try:
+            str1,number,title,link = text.split(" ")
+        except ValueError:
+            self.status.setText("Failed")
+        else:
+            cb = QGuiApplication.clipboard()
+            cb.setText(link)
+            self.status.setText("Done")
+
+if __name__ == "__main__":
+    app = QApplication()
+    dia = SliceDia()
+    dia.show()
+    app.exec()
+        
+        
