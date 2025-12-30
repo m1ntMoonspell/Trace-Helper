6aa9143328ee2264f2fde131e56c85407d4f32a5
m1ntMoonspell <zy1752706779@gmail.com>
2025-12-30 10:18:07 +0800

    started project

---
+++ main.py
@@ -0,0 +1,5 @@
+from PySide6.QtWidgets import QApplication
+
+if __name__ == "__main__":
+    app = QApplication()
+    app.exec()
---
+++ main_window.py
@@ -0,0 +1,5 @@
+from PySide6.QtWidgets import QMainWindow
+
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
